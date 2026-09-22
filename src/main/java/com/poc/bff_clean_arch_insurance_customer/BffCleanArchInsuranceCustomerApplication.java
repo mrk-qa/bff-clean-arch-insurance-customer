12083e19a6012db24e368eb745ba1dffb95bf4d5
Marco <marco.ant.seg@gmail.com>
2026-09-22 20:37:06 -0300

    setup bff

---
+++ src/main/java/com/poc/bff_clean_arch_insurance_customer/BffCleanArchInsuranceCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.bff_clean_arch_insurance_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffCleanArchInsuranceCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffCleanArchInsuranceCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/bff_clean_arch_insurance_customer/BffCleanArchInsuranceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.bff_clean_arch_insurance_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffCleanArchInsuranceCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
